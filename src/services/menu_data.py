4f4e9fa77ff3d57de821af939c9510c7a68938be
no_name <no@email.com>
2024-11-16 05:28:18 -0300

    Requisito 3 satisfeito

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,23 @@
 # Req 3
+import csv
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        with open(source_path, 'r', encoding='utf8') as file:
+            content_file = csv.reader(file)
+            next(content_file)
+            aux_dishes = dict()
+            for dish, price, ingredient, recipe_amount in content_file:
+                dish = Dish(dish, float(price))
+                if dish not in aux_dishes:
+                    aux_dishes[dish] = dish
+                else:
+                    # dish != aux_dishes -> pois são instâncias diferentes.
+                    dish = aux_dishes[dish]
+                dish.add_ingredient_dependency(
+                    Ingredient(ingredient), int(recipe_amount)
+                )
+            self.dishes = set(aux_dishes)
