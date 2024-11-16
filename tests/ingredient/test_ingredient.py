50bedea43db44cb3a74d6e02dc55b7afb6ecd510
no_name <no@email.com>
2024-11-16 00:43:07 -0300

    Requisito 1 satisfeito

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, restriction_map  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    restrictions = restriction_map()
+    egg = Ingredient('ovo')
+    egg2 = Ingredient('ovo')
+    assert egg.restrictions == restrictions['ovo']
+    assert egg == egg2
+    assert egg == egg
+    assert egg.name == 'ovo'
+    bacon = Ingredient('bacon')
+    assert egg != bacon
+    invalid_ingredient = Ingredient('salsa')
+    assert invalid_ingredient.restrictions == set()
+    assert hash(egg) == hash('ovo')
+    assert repr(egg) == "Ingredient('ovo')"
